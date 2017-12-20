808a103d4a28ea12935ec30d26917cab2b56404b
Erdal Yildiz <e7dal@users.noreply.github.com>
2017-12-20 15:05:25 +0100

    Update get_meta.sh

--- get_meta.sh
+++ get_meta.sh
@@ -1,4 +1,3 @@
-ROOTKEY=$(docker exec -it weaviate cat /var/weaviate/first_run.log|grep -s ROOTKEY|sed 's/.*ROOTKEY=//')"
+ROOTKEY=$(docker exec -it weaviate cat /var/weaviate/first_run.log|grep -s ROOTKEY|sed 's/.*ROOTKEY=//')
 echo $ROOTKEY
-curl -v -H \"X-API-KEY:$cat ROOTKEY.TXT" -X GET "http://127.0.0.1:8070/weaviate/v1/meta" -H  "accept: application/json" 
-  
+curl -v -H "X-API-KEY:$ROOTKEY" -H  "accept: application/json"  http://127.0.0.1:8070/weaviate/v1/meta 
